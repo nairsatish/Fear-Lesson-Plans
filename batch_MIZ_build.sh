9b32be664768f0e35426951255adb1bca0595e53
gregglickert <gglick9@gmail.com>
2021-05-28 11:04:28 -0500

    batch files for nairs

---
+++ batch_MIZ_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --qos=normal
+#SBATCH --job-name=10cell
+#SBATCH --output=build10Cell%j.out
+#SBATCH --time 0-00:30
+
+echo "building 10 cell model at $(date)"
+
+rm -rf network
+
+python3 build_network.py
+
+echo "Done building 10 cell model at $(data)"
+
---
+++ batch_MIZ_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --qos=normal
+#SBATCH --job-name=10Cell
+#SBATCH --output=Run10Cell.out
+#SBATCH --time 0-02:00
+
+rm -rf output
+
+echo "Running 10 cell model at $(date)"
+
+mpiexec nrniv -mpi -quiet -python run_save_network.py simulation_config.json
+
+echo "Done running 10 cell model at $(date)"
