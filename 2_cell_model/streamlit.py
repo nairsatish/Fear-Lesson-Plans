bfc66fe14e3fe2395ffce418b3ff5279657215c2
GregGlickert <59316745+GregGlickert@users.noreply.github.com>
2021-12-14 14:09:05 -0600

    Update streamlit.py

--- 2_cell_model/streamlit.py
+++ 2_cell_model/streamlit.py
@@ -589,7 +589,9 @@ st.write("""
 """)
 
 if st.button("build model"):
-    os.chdir("2_cell_model")
+    path = os.getcwd()
+    if (path!="2_cell_model);
+        os.chdir("2_cell_model")
     if os.path.isdir('biophys_components/mechanisms/x86_64'):
         shutil.rmtree('biophys_components/mechanisms/x86_64')
     os.chdir('biophys_components/mechanisms')
